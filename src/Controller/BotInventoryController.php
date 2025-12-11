f5ef093c8f81d89f9e031ddf0e0f447931b76297
Miles280 <loughsala.imed@gmail.com>
2025-12-11 05:53:38 +0100

    [DONE] Création de l'endpoint pour vendre l'un de ses items à un autre joueur

--- src/Controller/BotInventoryController.php
+++ src/Controller/BotInventoryController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Item;
 use App\Service\Auth\DiscordUserManager;
 use App\Service\InventoryService;
 use App\Service\RequestPayloadService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,14 +15,11 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/bot/inventory')]
 final class BotInventoryController extends AbstractController
 {
-    private InventoryService $inventoryService;
-    private DiscordUserManager $discordUserService;
-
-    public function __construct(InventoryService $inventoryService, DiscordUserManager $discordUserService)
-    {
-        $this->inventoryService = $inventoryService;
-        $this->discordUserService = $discordUserService;
-    }
+    public function __construct(
+        private InventoryService $inventoryService, 
+        private DiscordUserManager $discordUserService, 
+        private EntityManagerInterface $em
+    ) {}
 
     #[Route('/view', name: 'app_bot_inventory_view', methods: ['POST'])]
     public function view(Request $request, RequestPayloadService $payloadService): JsonResponse
@@ -36,5 +35,27 @@ final class BotInventoryController extends AbstractController
         return $this->json([
             'items' => $items
         ]);
+    }
+
+    #[Route('/sell', name: 'app_bot_inventory_sell', methods: ['POST'])]
+    public function sell(Request $request, RequestPayloadService $payloadService): JsonResponse
+    {
+        // Extraction et validation des données JSON envoyées par le bot
+        $payload = $payloadService->extractValidatedPayload($request, ['sellerId','buyerId','itemId', 'currency', 'price']);
+        if ($payload instanceof JsonResponse) return $payload;
+
+        $seller = $this->discordUserService->findOrCreateUserByDiscordId($payload['sellerId']);
+        $buyer = $this->discordUserService->findOrCreateUserByDiscordId($payload['buyerId']);
+        $item = $this->em->getRepository(Item::class)->find($payload['itemId']);
+        $currency = $payload['currency'];
+
+        if (!$item) {
+            return $this->json(['error' => "Item introuvable."], 404);
+        }
+
+        $result = $this->inventoryService->sellItem($seller, $buyer, $item, $currency, (int)$payload['price']
+        );
+
+        return $this->json($result);
     }
 }
--- src/Enum/TransactionType.php
+++ src/Enum/TransactionType.php
@@ -6,10 +6,13 @@ enum TransactionType: string
 {
     case GAIN = 'gain';
     case LOSE = 'lose';
-    case PURCHASE = 'purchase';
     case DONATION = 'donation';
     case RECEIVE = 'receive';
+    case ADD = 'add';
+    case REMOVE = 'remove';
+    case PURCHASE = 'purchase';
+    case SELL = 'sell';
+    case SET = 'set';
     case CONVERSION = 'conversion';
     case ADMIN = 'admin';
-    case SET = 'set';
 }

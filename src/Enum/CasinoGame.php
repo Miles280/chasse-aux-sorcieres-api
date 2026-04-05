0345f21decbd9462149085ee44fe70ed01f00bb3
Miles280 <loughsala.imed@gmail.com>
2026-04-05 23:10:39 +0200

    [FIX] Correction d'une erreur dans Enum CasinoGame

--- src/Enum/CasinoGame.php
+++ src/Enum/CasinoGame.php
@@ -5,7 +5,7 @@ namespace App\Enum;
 enum CasinoGame: string
 {
     case ROULETTE = 'roulette';
-    case BLACKJACK = 'blacjack';
+    case BLACKJACK = 'blackjack';
     case TOWER = 'tower';
     case MORE_OR_LESS = 'more_or_less';
 }
